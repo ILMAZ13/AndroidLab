4f55a308008c1ceca264bf0713222ca73f43b729
ILMAZ13 <ilmaz1309@gmai.com>
2017-03-10 20:09:28 +0300

    added MVP Pattern Application

---
+++ PatternMVP/app/src/main/java/com/study/ilmaz/patternmvp/view/PeopleActivity.java
@@ -0,0 +1,77 @@
+package com.study.ilmaz.patternmvp.view;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import com.study.ilmaz.patternmvp.R;
+import com.study.ilmaz.patternmvp.model.entities.User;
+import com.study.ilmaz.patternmvp.presenter.PeoplePresenter;
+import com.study.ilmaz.patternmvp.view.interfaces.PeopleActivityInterface;
+
+import java.util.List;
+
+public class PeopleActivity extends AppCompatActivity implements PeopleActivityInterface{
+    private PeoplePresenter presenter;
+    private RecyclerView recyclerView;
+    private FloatingActionButton button;
+    private PeopleAdapter peopleAdapter;
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_people);
+
+        presenter = new PeoplePresenter(this);
+
+        initViews();
+        setupRecyclerView();
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                presenter.onLoadButtonPressed(v);
+            }
+        });
+    }
+
+    private void setupRecyclerView() {
+        peopleAdapter = new PeopleAdapter();
+        peopleAdapter.setItemClickListener(new PeopleAdapter.ItemClickListener() {
+            @Override
+            public void onItemClick(User user, View view) {
+                presenter.onItemClicked(user, view);
+            }
+        });
+        recyclerView.setAdapter(peopleAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    private void initViews() {
+        recyclerView = (RecyclerView) findViewById(R.id.rv_people);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        button = (FloatingActionButton) findViewById(R.id.fab_add);
+    }
+
+    @Override
+    public void showPeopleList(List<User> userList) {
+        progressBar.setVisibility(View.GONE);
+        peopleAdapter.setUserList(userList);
+        peopleAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void showLoading() {
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void navigateToDetailActivity(User user, View view) {
+        startActivity(DetailActivity.launchDetail(view.getContext(), user));
+    }
+}
